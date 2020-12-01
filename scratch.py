ce7a6434b7b92f0507bdcdcfecd969532824a4f8
TheHuessy <jameshuessy@gmail.com>
2020-12-01 17:00:26 -0500

    java library problems

--- scratch.py
+++ scratch.py
@@ -1,23 +1,37 @@
 ## Will need to source spark env vars prior to running
 
 import os
+import sys
 from pyspark import SparkContext
+from pyspark import SQLContext
 
 ## For most data transformation think of each function as being applied to a row
-def clean_spark_data(row_data):
-    return([clean_row.replace('"', '') for clean_row in row_data])
+#def prepare_raw_spark_data(row_data):
+#    clean_rows = [clean_row.replace('"', '') for clean_row in row_data]
+#    print(clean_rows)
+#    print(dir(clean_rows))
+#    return(clean_rows)
+#    return([clean_row.replace('"', '') for clean_row in row_data])
 
 ## Start spark context
 sc = SparkContext(master='yarn', appName="R-Log-Test")
 
+## Start spark SQL context
+sqlsc = SQLContext(sc)
+
 ## Pull in text data from hdfs
-data = sc.textFile("{}/user/pi/pyspark_practice/2020-11-01.csv".format(os.environ['NAMENODE_PATH']))
+rows = sc.textFile("{}/user/pi/pyspark_practice/2020-11-01.csv".format(os.environ['NAMENODE_PATH'])).map(lambda x: x.replace('"', '')).map(lambda x: x.split(','))
 
-## Split data into a series of row 'arrays'
-rows = data.map(lambda x: x.split(',')).map(clean_spark_data)
+## Create the dataframe from the cleaned RDD object
+full_df = sqlsc.createDataFrame(data = rows.filter(lambda x: x[0] != 'date'),
+        schema=rows.filter(lambda x: x[0] == 'date').collect()[0])
 
-date_freq = rows.map(lambda x: (x[0], 1)).filter(lambda x: x[0] != 'date').countByKey()
+## Cache the df in RAM for easier transformation and calling
+full_df.persist()
 
-print(date_freq)
+#Have a look:
+#sql_df.show(10)
+
+full_df.groupBy("package").count().sort("count", ascending = False).show(10)
 
 
